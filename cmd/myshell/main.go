ee1abd228d4bd2b82bbc2b6afef954879db687f4
chetra-seng <dev@chetraseng.com>
2025-02-02 12:31:50 +0700

    codecrafters submit [skip ci]

--- cmd/myshell/main.go
+++ cmd/myshell/main.go
@@ -11,8 +11,11 @@ var _ = fmt.Fprint
 
 func main() {
 	// Uncomment this block to pass the first stage
-	// fmt.Fprint(os.Stdout, "$ ")
+  for {
+
+	fmt.Fprint(os.Stdout, "$ ")
 
 	// Wait for user input
 	bufio.NewReader(os.Stdin).ReadString('\n')
+  }
 }
