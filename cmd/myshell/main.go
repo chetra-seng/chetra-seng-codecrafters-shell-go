ebb851d54752fbaa7a9ac7bfd83b9ea658d19751
chetra-seng <dev@chetraseng.com>
2025-02-02 13:23:19 +0700

    codecrafters submit [skip ci]

--- cmd/myshell/main.go
+++ cmd/myshell/main.go
@@ -10,14 +10,16 @@ import (
 var _ = fmt.Fprint
 
 func main() {
-	fmt.Fprint(os.Stdout, "$ ")
 
-	// Wait for user input
-  command, err := bufio.NewReader(os.Stdin).ReadString('\n')
+	for {
+		fmt.Fprint(os.Stdout, "$ ")
+		// Wait for user input
+		command, err := bufio.NewReader(os.Stdin).ReadString('\n')
 
-  if err != nil {
-    fmt.Println("Error reading input", err)
-  }
+		if err != nil {
+			fmt.Println("Error reading input", err)
+		}
 
-  fmt.Println(command[:len(command)-1] + ": command not found")
+		fmt.Println(command[:len(command)-1] + ": command not found")
+	}
 }
